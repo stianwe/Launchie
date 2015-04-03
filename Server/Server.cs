f2d3096d75023bbee2d476b7c596a7465f06a5b3
Stian Weie <stianwe@gmail.com>
2015-04-03 14:27:29 +0200

    Initial server code

---
+++ Server/Server.cs
@@ -0,0 +1,19 @@
+﻿using Launchie;
+
+namespace Server
+{
+    public class Server
+    {
+        private string _rootDir;
+
+        public Server(string rootDir)
+        {
+            _rootDir = rootDir;
+        }
+
+        public void ComputeFileHashes()
+        {
+            Hasher.GetDirectoryHash(_rootDir, true);
+        }
+    }
+}
